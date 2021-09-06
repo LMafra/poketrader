2c07ae8d3d2eb8de828ba8e8ef17a523cb3e979c
LMafra <chagas.lucas.mafra@gmail.com>
2021-09-06 05:20:33 -0300

    Find problem. Trying new solution pt2

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,11 @@ app.use(express.json());
 app.use(haltOnTimedout);
 app.use(routes);
 app.use(haltOnTimedout);
-app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(haltOnTimedout);
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 
 module.exports = app;
