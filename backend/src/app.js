0bfd816242db225afce2148cd5d59c8c85993d81
LMafra <chagas.lucas.mafra@gmail.com>
2021-09-06 04:39:29 -0300

    Add timeout to try to fix heroku

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const serveStatic = require('serve-static');
 const path = require('path');
+const timeout = require('connect-timeout');
 const routes = require('./routes');
 
 const app = express();
 
+function haltOnTimedout(req, res, next) {
+  if (!req.timedout) next();
+}
+
+app.use(timeout('5s'));
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(haltOnTimedout);
 
 app.get('/', (req, res) => {
   res.status(200);
