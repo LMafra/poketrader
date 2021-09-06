d6d2ee7da0d1ada6d61f94a305189eaeb435ccef
LMafra <chagas.lucas.mafra@gmail.com>
2021-09-06 04:28:06 -0300

    Fix deploy pt15

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const serveStatic = require('serve-static');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -7,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
   res.status(200);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,4 @@
-const serveStatic = require('serve-static');
-const path = require('path');
 const app = require('./app');
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3333;
 app.listen(port);
