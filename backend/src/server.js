e210d981e78326d1fd34d12768b73ce761059f6d
LMafra <chagas.lucas.mafra@gmail.com>
2021-09-06 00:57:50 -0300

    Fix deploy pt10

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
-const serveStatic = require("serve-static");
+const serveStatic = require('serve-static');
 const path = require('path');
 const app = require('./app');
-
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3333;
