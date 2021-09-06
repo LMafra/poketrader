d54d14780127442b4a5684826f4acd738e7edd34
LMafra <chagas.lucas.mafra@gmail.com>
2021-09-06 06:15:53 -0300

    The solution. Question mark

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,12 +5,6 @@ const TradeController = require('./controllers/tradeController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  res.status(200).send({
-    title: 'Node Express API',
-    version: '0.0.1',
-  });
-});
 routes.get('/pokemons', PokemonController.index);
 routes.post('/pokemons/:name', PokemonController.create);
 
